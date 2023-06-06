8e541605b4f2ac0ee240ea6dc1166b172af0dd42
Bartosz <kwiatkowsky.kontakt@gmail.com>
2023-06-06 15:05:30 +0200

    chore: initialize project

---
+++ src/main/java/com/kwiatkowsky/Distances/DistancesApplication.java
@@ -0,0 +1,13 @@
+package com.kwiatkowsky.Distances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwiatkowsky/Distances/DistancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwiatkowsky.Distances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
